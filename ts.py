ad2d34e111be59682d6509bcb4c0235ca280cf78
imaustinim <36931656+imaustinim@users.noreply.github.com>
2021-03-12 10:51:17 -0500

    Delete ts.py

--- ts.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-print('\n'.join(sys.path))
-
-# sys.path.insert(0, '/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7')
-# export PYTHONPATH = "/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7"
